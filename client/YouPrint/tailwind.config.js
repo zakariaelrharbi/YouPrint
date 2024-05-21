6ea14a9f5da1618a1c5a58c36860cd9302a8e4fc
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-05-21 12:42:05 +0100

    setup talwind and choosing font

--- client/YouPrint/tailwind.config.js
+++ client/YouPrint/tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#FF6363",
+        "secondary": {
+          100: "#E2E2D5",
+          200: "#888883",
+        },
+      },
+    },
   },
   plugins: [],
 }
-
