999e4045f13060b20d6deb960a9d1d4d4997c6a7
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-05-22 17:54:17 +0100

    update hero section

--- client/YouPrint/src/Pages/Home.jsx
+++ client/YouPrint/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
+import Cards from '../components/Cards'
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
+      <Cards />
     </div>
   )
 }
---
+++ client/YouPrint/src/components/Cards.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const CardData = [
+  {
+    title: "Lorem Ipsum Dolor",
+    img: "https://readymadeui.com/cardImg.webp",
+  },
+  {
+    title: "Lorem Ipsum Dolor",
+    img: "https://readymadeui.com/cardImg.webp",
+  },
+  {
+    title: "Lorem Ipsum Dolor",
+    img: "https://readymadeui.com/cardImg.webp",
+  },
+  {
+    title: "Lorem Ipsum Dolor",
+    img: "https://readymadeui.com/cardImg.webp",
+  },
+];
+
+const Cards = () => {
+  return (
+    <div className="bg-gray-100 md:px-10 px-4 py-12 font-[sans-serif]">
+      <div className="container mx-auto">
+        <h2 className="text-3xl font-extrabold text-center text-gray-800 mb-8">
+          Categories
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+          {CardData.map((data, index) => (
+            <div key={index} className="bg-white rounded-3xl overflow-hidden shadow-lg">
+              <div className="p-6">
+                <h3 className="text-xl font-bold text-gray-800 text-center">
+                  {data.title}
+                </h3>
+              </div>
+              <img
+                src={data.img}
+                alt={data.title}
+                className="w-full h-52 rounded-xl mb-14 p-2 object-cover"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
+
