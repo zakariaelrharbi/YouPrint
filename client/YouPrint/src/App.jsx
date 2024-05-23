7186fcf597575c583d7d0c8876b7d415b093ce33
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-05-23 14:30:53 +0100

    adding header and footer

--- client/YouPrint/src/App.jsx
+++ client/YouPrint/src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import Home from './Pages/Home' // Import the Home component
 
 const App = () => {
   return (
     <>
     <Header />
-    <Outlet />
+      <main>
+        <Outlet />
+      </main>
     <Footer />
     </>
   )
--- client/YouPrint/src/routes/index.jsx
+++ client/YouPrint/src/routes/index.jsx
@@ -1,10 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import Home from "../Pages/Home";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
+        children: [
+            { path: "/", element: <Home /> },
+        ],
     },
 ]);
 
