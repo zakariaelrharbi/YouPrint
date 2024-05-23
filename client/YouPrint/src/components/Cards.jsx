e70ac7aacd15755ae852ee664b1744e640a148bd
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-05-23 13:17:03 +0100

    adding img to cards

--- client/YouPrint/src/components/Cards.jsx
+++ client/YouPrint/src/components/Cards.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import img1 from '../assets/Images/imgA.png'
+
+
 
 const CardData = [
   {
     title: "Lorem Ipsum Dolor",
-    img: "https://readymadeui.com/cardImg.webp",
+    img: img1,
   },
   {
     title: "Lorem Ipsum Dolor",
-    img: "https://readymadeui.com/cardImg.webp",
+    img: img1,
   },
   {
     title: "Lorem Ipsum Dolor",
@@ -29,7 +32,7 @@ const Cards = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {CardData.map((data, index) => (
             <div key={index} className="bg-white rounded-3xl overflow-hidden shadow-lg">
-              <div className="p-6">
+              <div className="p-2">
                 <h3 className="text-xl font-bold text-gray-800 text-center">
                   {data.title}
                 </h3>
@@ -37,7 +40,7 @@ const Cards = () => {
               <img
                 src={data.img}
                 alt={data.title}
-                className="w-full h-52 rounded-xl mb-14 p-2 object-cover"
+                className="w-full  rounded-xl  p-2 object-cover"
               />
             </div>
           ))}
