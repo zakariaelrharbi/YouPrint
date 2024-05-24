476449cc81196bc56e86bbbb7349f35afcd9167c
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-05-24 16:56:46 +0100

    create backend after reset

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const dotenv = require("dotenv");
 const connect = require('./config/database');
+const cors = require('cors');
+const router = require('./routes/index');
 dotenv.config();
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/api', router);
+
+
 const PORT = process.env.SERVER_PORT || 5000;
 
 async function main() {
