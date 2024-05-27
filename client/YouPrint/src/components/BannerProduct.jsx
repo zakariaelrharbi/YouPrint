5a6ebd1a3397180bef66138339ffc764c69daf82
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-05-27 10:41:07 +0100

    adding more image to banner product

--- client/YouPrint/src/components/BannerProduct.jsx
+++ client/YouPrint/src/components/BannerProduct.jsx
@@ -1,14 +1,43 @@
 import React from 'react'
 import image1 from '../assets/Banner/img1.webp'
+import image2 from '../assets/Banner/img2.webp'
+import image3 from '../assets/Banner/img3.jpg'
+import image4 from '../assets/Banner/img4.jpg'
+import image5 from '../assets/Banner/img5.webp'
+
 import image1M from '../assets/Banner/img1_mobile.jpg'
+import image2M from '../assets/Banner/img2_mobile.webp'
+import image3M from '../assets/Banner/img3_mobile.jpg'
+import image4M from '../assets/Banner/img4_mobile.jpg'
+import image5M from '../assets/Banner/img5_mobile.png'
 
 const BannerProduct = () => {
+    const desktopImages = [
+        image1,
+        image2,
+        image3,
+        image4,
+        image5,
+    ]
+    const mobileImages = [
+        image1M,
+        image2M,
+        image3M,
+        image4M,
+        image5M,
+    ]
   return (
-    <div className='container mx-auto px-4 rounded overflow-hidden '>
+    <div className='container mx-auto px-4 rounded  '>
         <div className='h-72 w-full bg-slate-200'>
-            <div className='w-full h-full'>
-                <img src={image1} className='w-full h-full'/>
-            </div>
+            {
+                desktopImages.map((imageURL, index) => {
+                    return (
+                        <div className='w-full h-full' key={imageURL}> 
+                            <img src={imageURL} className='w-full h-full'/>
+                        </div>
+                    )
+                })
+            }
         </div>
     </div>
   )
