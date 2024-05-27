0ad160a609712f9f81f8b12ed76767a977bf669f
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-05-27 09:49:53 +0100

    start creating bannerproduct

--- client/YouPrint/src/components/BannerProduct.jsx
+++ client/YouPrint/src/components/BannerProduct.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import image1 from '../assets/Banner/img1.webp'
+import image1M from '../assets/Banner/img1_mobile.jpg'
 
 const BannerProduct = () => {
   return (
     <div className='container mx-auto px-4 rounded overflow-hidden'>
-        <div className='h-72 w-full bg-red-600'>
-
+        <div className='h-72 w-full bg-slate-200'>
+            <img src={image1} />
         </div>
     </div>
   )
