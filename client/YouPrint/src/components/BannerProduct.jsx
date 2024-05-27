76360b6b9e5449899d1f91c0ffc43d8f17778705
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-05-27 11:41:52 +0100

    adding next and previous button

--- client/YouPrint/src/components/BannerProduct.jsx
+++ client/YouPrint/src/components/BannerProduct.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FaAngleRight } from "react-icons/fa6";
+import { FaAngleLeft } from "react-icons/fa6";
+
+
+
 import image1 from '../assets/Banner/img1.webp'
 import image2 from '../assets/Banner/img2.webp'
 import image3 from '../assets/Banner/img3.jpg'
@@ -12,6 +17,8 @@ import image4M from '../assets/Banner/img4_mobile.jpg'
 import image5M from '../assets/Banner/img5_mobile.png'
 
 const BannerProduct = () => {
+    const [currentImage, setCurrentImage] = useState(0)
+
     const desktopImages = [
         image1,
         image2,
@@ -26,18 +33,39 @@ const BannerProduct = () => {
         image4M,
         image5M,
     ]
+
+    const nextImage = () => {
+        if( desktopImages.length - 1 > currentImage){
+            setCurrentImage(preve => preve + 1)
+        } 
+    }
+    const prevImage = () => {
+        if(currentImage !== 0){
+            setCurrentImage(preve => preve - 1)
+        } 
+    }
+
   return (
     <div className='container mx-auto px-4 rounded  '>
-        <div className='h-72 w-full bg-slate-200'>
-            {
-                desktopImages.map((imageURL, index) => {
-                    return (
-                        <div className='w-full h-full' key={imageURL}> 
-                            <img src={imageURL} className='w-full h-full'/>
-                        </div>
-                    )
-                })
-            }
+        <div className='h-72 w-full bg-slate-200 relative'>
+            <div className='absolute z-10 h-full w-full flex items-center p-2'>
+                <div className=' flex justify-between w-full text-2xl '>
+                <button className='bg-white shadow-md rounded-full p-1 hover:scale-105'onClick={prevImage} ><FaAngleLeft /></button>
+                <button className='bg-white shadow-md rounded-full p-1 hover:scale-105'onClick={nextImage}><FaAngleRight /></button>
+                </div>
+            </div>
+
+            <div className='flex h-full w-full overflow-hidden '>
+                {
+                    desktopImages.map((imageURL, index) => {
+                        return (
+                            <div className='w-full h-full min-h-full min-w-full' key={imageURL} style={{transform : `translateX(-${currentImage * 100}%)`}}> 
+                                <img src={imageURL} className='w-full h-full'/>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     </div>
   )
