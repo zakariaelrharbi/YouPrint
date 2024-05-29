7b4c9fb3603cff1a3c6da300e21f67df5aff4646
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-05-29 17:14:57 +0100

    custimization error and success messages in signup

--- server/controllers/userSignup.js
+++ server/controllers/userSignup.js
@@ -17,14 +17,22 @@ const userSignup = async (req, res) => {
 
         const existingUser = await User.findOne({ email });
         if (existingUser) {
-            return res.status(400).json({ message: 'User already exists' });
+            return res.status(400).json({ 
+                message: 'User already exists',
+                error: true,
+                success: false,
+             });
         }
 
         const hashedPassword = bcrypt.hashSync(password, 10);
         const newUser = new User({ username, email, password: hashedPassword });
 
         await newUser.save();
-        res.status(201).json({ message: 'User created successfully' });
+        res.status(201).json({
+            message: 'User created successfully',
+            error: false,
+            success: true,
+        });
 
     } catch (error) {
         res.json({ message: error,
