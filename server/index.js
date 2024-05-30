57bf033b3831715e5ecaa584e30766c478f8d5b8
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-05-30 12:39:04 +0100

    update cors value

--- server/index.js
+++ server/index.js
@@ -7,7 +7,11 @@ const cookieParser = require('cookie-parser');
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+}
+));
 app.use(express.json());
 app.use('/api', router);
 app.use(cookieParser());
