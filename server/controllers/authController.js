aa079fc17ac64859894e9e278c84c2b9f18046a1
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-06-03 17:06:24 +0100

    adding google auth function

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -4,7 +4,6 @@ const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 dotenv.config();
 
-
 const userSignup = async (req, res) => {
     const { username, email, password } = req.body;
 
@@ -13,7 +12,7 @@ const userSignup = async (req, res) => {
             message: 'All fields are required',
             error: true,
             success: false,
-         });
+        });
     }
 
     const existingUser = await User.findOne({ email });
@@ -24,13 +23,14 @@ const userSignup = async (req, res) => {
             success: false,
         });
     }
-    const hashedPassword = bcrypt.hashSync(password, 10);
-    const newUser = new User({
-        username,
-        email,
-        password: hashedPassword,
-    });
-    try{
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword,
+        });
         await newUser.save();
         return res.status(200).json({
             message: 'User created successfully',
@@ -44,8 +44,7 @@ const userSignup = async (req, res) => {
             success: false,
         });
     }
-
-}
+};
 
 const userSignin = async (req, res) => {
     const { email, password } = req.body;
@@ -68,7 +67,7 @@ const userSignin = async (req, res) => {
             });
         }
 
-        const validPassword = bcrypt.compareSync(password, validUser.password);
+        const validPassword = await bcrypt.compare(password, validUser.password);
         if (!validPassword) {
             return res.status(400).json({
                 message: 'Invalid password',
@@ -81,7 +80,7 @@ const userSignin = async (req, res) => {
 
         const { password: userPassword, ...rest } = validUser._doc;
         return res.status(200)
-            .cookie('access_token', token, { httpOnly: true })
+            .cookie('access_token', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production' })
             .json({
                 user: rest,
                 message: 'Signin successful',
@@ -96,6 +95,51 @@ const userSignin = async (req, res) => {
             success: false,
         });
     }
-}
+};
 
-module.exports = {userSignup, userSignin}
+const google = async (req, res) => {
+    const { name, email, googlePhotoURL } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (user) {
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+            const { password, ...rest } = user._doc;
+            return res.status(200)
+                .cookie('access_token', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production' })
+                .json({
+                    user: rest,
+                    message: 'Signin successful',
+                    error: false,
+                    success: true,
+                });
+        } else {
+            const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+            const hashedPassword = await bcrypt.hash(generatedPassword, 10);
+            const newUser = new User({
+                username: name.toLowerCase().split(' ').join('') + Math.random().toString(9).slice(-4),
+                email,
+                password: hashedPassword,
+                profilePicture: googlePhotoURL,
+            });
+            await newUser.save();
+            const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+            const { password, ...rest } = newUser._doc;
+            return res.status(200)
+                .cookie('access_token', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production' })
+                .json({
+                    user: rest,
+                    message: 'Signin successful',
+                    error: false,
+                    success: true,
+                });
+        }
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Server error',
+            error: true,
+            success: false,
+        });
+    }
+};
+
+module.exports = { userSignup, userSignin, google };
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { userSignup, userSignin} = require('../controllers/authController');
+const { userSignup, userSignin, google} = require('../controllers/authController');
 
 const router = express.Router();
 
 router.post('/signup', userSignup);
 router.post('/signin', userSignin);
+router.post('/google', google);
+
 
 
 module.exports = router;
