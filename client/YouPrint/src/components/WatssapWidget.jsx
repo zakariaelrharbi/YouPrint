cfbbba5a3302c069df8db150c24062085db8f54c
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-06-24 13:07:10 +0100

    fixing whatssap size for mobile size

--- client/YouPrint/src/components/WatssapWidget.jsx
+++ client/YouPrint/src/components/WatssapWidget.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-            
+
 const WhatsappWidget = () => {
   return (
     <div>
       {/* WhatsApp icon */}
       <a
         href="https://wa.me/2348100000000"
-        className="fixed w-[40px] h-[40px] xl:w-[65px] xl:h-[65px] bottom-[20px] right-[10px] xl:bottom-[20px] xl:right-[30px] bg-[#25d366] text-white rounded-full text-center shadow-custom-green z-[100] hover:scale-110 transition-all duration-300"
+        className="fixed w-[50px] h-[50px] xl:w-[65px] xl:h-[65px] bottom-[20px] right-[10px] xl:bottom-[20px] xl:right-[30px] bg-[#25d366] text-white rounded-full text-center shadow-custom-green z-[100] hover:scale-110 transition-all duration-300"
         target="_blank"
         rel="noopener noreferrer"
       >
