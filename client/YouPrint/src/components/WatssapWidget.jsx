a1a91cfe54cf736527263c37df3b60f4bdf49563
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-06-24 10:41:34 +0100

    adding watssap widget

--- client/YouPrint/src/components/WatssapWidget.jsx
+++ client/YouPrint/src/components/WatssapWidget.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import watssapIcon from '../assets/Images/whatsapp.png'
 
 const WatssapWidget = () => {
   return (
@@ -9,7 +8,7 @@ const WatssapWidget = () => {
       <a
         href="https://wa.me/2348100000000"
         // whatsapp_float
-        className="fixed w-[60px] h-[60px] bottom-[40px] right-[40px] bg-[#25d366] text-white rounded-full text-center text-[30px] shadow-lg z-[100]"
+        className="fixed w-[40px] h-[40px] xl:w-[60px] xl:h-[60px] bottom-[20px] right-[10px] xl:bottom-[20px] xl:right-[30px] bg-[#25d366] text-white rounded-full text-center text-[30px] shadow-xl z-[100] hover:scale-110 transition-all duration-300"
         target="_blank"
         rel="noopener noreferrer"
       >
