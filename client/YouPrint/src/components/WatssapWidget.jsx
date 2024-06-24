0ad13b8da74f08eeebfb7800bf5466ab9d8a6e1c
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-06-24 11:01:37 +0100

    adding watssap widget

--- client/YouPrint/src/components/WatssapWidget.jsx
+++ client/YouPrint/src/components/WatssapWidget.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaWhatsapp  } from "react-icons/fa";
 
 const WhatsappWidget = () => {
   return (
@@ -6,11 +7,11 @@ const WhatsappWidget = () => {
       {/* WhatsApp icon */}
       <a
         href="https://wa.me/2348100000000"
-        className="fixed w-[40px] h-[40px] xl:w-[60px] xl:h-[60px] bottom-[20px] right-[10px] xl:bottom-[20px] xl:right-[30px] bg-[#25d366] text-white rounded-full text-center shadow-custom-green z-[100] hover:scale-110 transition-all duration-300"
+        className="fixed w-[40px] h-[40px] xl:w-[65px] xl:h-[65px] bottom-[20px] right-[10px] xl:bottom-[20px] xl:right-[30px] bg-[#25d366] text-white rounded-full text-center shadow-custom-green z-[100] hover:scale-110 transition-all duration-300"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <i className="fa fa-whatsapp text-[24px] xl:text-[30px] flex items-center justify-center h-full"></i>
+        <i className="fa fa-whatsapp text-[24px] xl:text-[36px] flex items-center justify-center h-full"></i>
       </a>
     </div>
   );
