5208ab95cd683bf3ff88f3c87244402c5806be02
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-06-24 12:46:57 +0100

    adding focus to searchbar input

--- client/YouPrint/src/components/SearchBar.jsx
+++ client/YouPrint/src/components/SearchBar.jsx
@@ -8,7 +8,7 @@ const SearchBar = () => {
         <input
           type="text"
           placeholder="Que recherchez-vous?"
-          className="placeholder-white/60 bg-transparent px-5 xl:pr-16 rounded-full p-2 text-sm focus:outline-none xl:ml-20 w-full xl:w-80 custom-input"
+          className="placeholder-white/60 bg-transparent px-5 xl:pr-16 rounded-full p-2 text-sm focus:outline-none xl:ml-20 w-full xl:w-80 focus:ring-primaryGreen focus:border-primaryGreen"
         />
         <button type="submit" className="absolute right-0 top-0 mt-2 mr-4">
           <CiSearch className="text-white text-xl" />
