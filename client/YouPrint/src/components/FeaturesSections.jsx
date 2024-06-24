6466f22e5b604b1ef5805f847940841b6ba22dcb
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-06-24 16:59:46 +0100

    fixing whatssap size for mobile size

---
+++ client/YouPrint/src/components/FeaturesSections.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { FaRecycle } from "react-icons/fa";
+
+
+
+
+
+
+
+const FeaturesSections = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FeaturesSections
