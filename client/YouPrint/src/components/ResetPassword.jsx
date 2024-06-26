ea3ff682e477683a9839744af9fe91ed83f48301
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-06-26 10:09:52 +0100

    adding password label

--- client/YouPrint/src/components/ResetPassword.jsx
+++ client/YouPrint/src/components/ResetPassword.jsx
@@ -1,47 +1,67 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { FaRegEyeSlash, FaRegEye } from "react-icons/fa";
 
 const ResetPassword = () => {
+      const [showPassword, setShowPassword] = useState(false);
+
+      const [data, setData] = useState({
+        password: "",
+    });
+    const handleOnChange = (e) => {
+        const { name, value } = e.target;
+
+        setData((prevData) => ({
+            ...prevData,
+            [name]: value.trim(),
+        }));
+    }
+      const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+      }
   return (
     <div>
       <section className="bg-gray-50 dark:bg-gray-900">
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
     <div className="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8">
       <h2 className="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-        Reset Password
+        Réinitialiser votre mot de passe
       </h2>
       <form className="mt-4 space-y-4 lg:mt-5 md:space-y-5" action="#">
         <div>
-          <label
-            htmlFor="password"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            New Password
-          </label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="••••••••"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            required=""
-          />
-        </div>
+          <label className="text-sm mb-2 block">Nouveau mot de passe<span className='text-red-500'> *</span></label>
+            <div className="relative flex items-center">
+               <input
+                name="password"
+                onChange={handleOnChange}
+                value={data.password}
+                type={showPassword ? "text" : "password"}
+                className="bg-white border border-gray-300 w-full text-sm px-4 py-2.5 rounded outline-primaryGreen focus:ring-primaryGreen focus:border-primaryGreen"
+                placeholder="********"
+                />
+                {showPassword ? (
+                  <FaRegEyeSlash onClick={togglePasswordVisibility} className="w-4 h-4 absolute right-4 cursor-pointer" />
+                  ) : (
+                   <FaRegEye onClick={togglePasswordVisibility} className="w-4 h-4 absolute right-4 cursor-pointer" />
+                  )}
+            </div>
+          </div>
         <div>
-          <label
-            htmlFor="confirm-password"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Confirm password
-          </label>
-          <input
-            type="confirm-password"
-            name="confirm-password"
-            id="confirm-password"
-            placeholder="••••••••"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            required=""
-          />
+          <label className="text-sm mb-2 block">Confirmer votre mot de passe<span className='text-red-500'> *</span></label>
+            <div className="relative flex items-center">
+               <input
+                name="password"
+                onChange={handleOnChange}
+                value={data.password}
+                type={showPassword ? "text" : "password"}
+                className="bg-white border border-gray-300 w-full text-sm px-4 py-2.5 rounded outline-primaryGreen focus:ring-primaryGreen focus:border-primaryGreen"
+                placeholder="********"
+                />
+                {showPassword ? (
+                  <FaRegEyeSlash onClick={togglePasswordVisibility} className="w-4 h-4 absolute right-4 cursor-pointer" />
+                  ) : (
+                   <FaRegEye onClick={togglePasswordVisibility} className="w-4 h-4 absolute right-4 cursor-pointer" />
+                  )}
+            </div>
         </div>
         <button
           type="submit"
