926ee1d124e888ab7b2df6d65c066441b6adba7b
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-06-26 13:40:33 +0100

    fixing cannot write in email input bug

--- client/YouPrint/src/components/ForgotPassword.jsx
+++ client/YouPrint/src/components/ForgotPassword.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-import { Button, Label, Modal, TextInput } from 'flowbite-react';
+import { Modal } from 'flowbite-react';
 import { toast } from 'react-toastify';
 import { MdOutlineMailOutline } from "react-icons/md";
-
 
 const ForgotPassword = ({ visible, onClose }) => {
   const [data, setData] = useState({
@@ -12,7 +11,7 @@ const ForgotPassword = ({ visible, onClose }) => {
   const handleChange = (e) => {
     setData({
       ...data,
-      [e.target.id]: e.target.value,
+      [e.target.name]: e.target.value,
     });
   };
 
@@ -42,7 +41,12 @@ const ForgotPassword = ({ visible, onClose }) => {
           email: data.email.trim(),
         }),
       });
-      
+
+      if (response.ok) {
+        toast.success('Password reset email sent successfully');
+      } else {
+        toast.error('Failed to send password reset email');
+      }
     } catch (error) {
       toast.error('An error occurred while resetting the password');
       console.error("Error during fetch:", error); // Debug log
@@ -57,26 +61,26 @@ const ForgotPassword = ({ visible, onClose }) => {
         Réinitialiser le mot de passe
       </Modal.Header>
       <Modal.Body>
-        <form onSubmit={handleSubmit} className="space-y-6 flex gap-4">
-            <div>
-              <label className="text-sm mb-1 block">Votre e-mail<span className='text-red-500'> *</span></label>
-                <div className="relative flex items-center">
-                  <input
-                   name="email"
-                    type="email"
-                    value={data.email}
-                    onChange={handleChange}
-                    className="bg-white border border-gray-300 w-[210px]  text-sm px-2 py-2 rounded outline-primaryGreen focus:ring-primaryGreen focus:border-primaryGreen"
-                    placeholder="nom@gmail.com"
-                    />
+        <form onSubmit={handleSubmit} className="space-y-6 flex gap-3">
+          <div>
+            <label className="text-sm mb-1 block">Votre e-mail<span className='text-red-500'> *</span></label>
+            <div className="relative flex items-center">
+              <input
+                name="email"
+                type="text"
+                value={data.email}
+                onChange={handleChange}
+                className="bg-white border border-gray-300 w-[250px] text-sm px-2 py-2 rounded outline-primaryGreen focus:ring-primaryGreen focus:border-primaryGreen"
+                placeholder="nom@gmail.com"
+              />
               <MdOutlineMailOutline className="w-4 h-4 absolute right-4" />
-                  </div>
-              </div>
+            </div>
+          </div>
           <button
             type="submit"
-            className='px-4 py-1 text-sm font-semibold rounded text-white bg-primaryGreen hover:opacity-90 focus:outline-none'
-            >
-             Se connecter
+            className='px-2 py-1 text-sm font-semibold rounded text-white bg-primaryGreen hover:opacity-90 focus:outline-none'
+          >
+            Se connecter
           </button>
         </form>
       </Modal.Body>
