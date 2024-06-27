c2d3eca318e80e23c4e886ec6802156288c3a0b5
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-06-27 16:18:22 +0100

    update

--- client/YouPrint/src/components/ForgotPassword.jsx
+++ client/YouPrint/src/components/ForgotPassword.jsx
@@ -56,35 +56,40 @@ const ForgotPassword = ({ visible, onClose }) => {
   if (!visible) return null;
 
   return (
-    <Modal show={visible} onClose={onClose} className='pt-[250px] xl:pt-0'>
-      <Modal.Header className='mb-0'>
-        Réinitialiser le mot de passe
-      </Modal.Header>
-      <Modal.Body>
-        <form onSubmit={handleSubmit} className="space-y-6 flex gap-3">
-          <div>
-            <label className="text-sm mb-1 block">Votre e-mail<span className='text-red-500'> *</span></label>
-            <div className="relative flex items-center">
-              <input
-                name="email"
-                type="text"
-                value={data.email}
-                onChange={handleChange}
-                className="bg-white border border-gray-300 w-[230px] text-sm px-2 py-2 rounded outline-primaryGreen focus:ring-primaryGreen focus:border-primaryGreen"
-                placeholder="nom@gmail.com"
-              />
-              <MdOutlineMailOutline className="w-4 h-4 absolute right-4" />
+
+    <div className=''>
+      <Modal show={visible} onClose={onClose} className='pt-[250px] xl:pt-0'>
+        <Modal.Header className='mb-0'>
+          Réinitialiser le mot de passe
+        </Modal.Header>
+        <Modal.Body>
+          <div className=''>
+          <form onSubmit={handleSubmit} className="space-y-6 flex gap-3">
+            <div className='xl:w-[200px]'>
+              <label className="text-sm mb-1 block">Votre e-mail<span className='text-red-500'> *</span></label>
+              <div className="relative flex items-center">
+                <input
+                  name="email"
+                  type="text"
+                  value={data.email}
+                  onChange={handleChange}
+                  className="bg-white border border-gray-300 w-[230px] text-sm px-2 py-2 rounded outline-primaryGreen focus:ring-primaryGreen focus:border-primaryGreen"
+                  placeholder="nom@gmail.com"
+                />
+                <MdOutlineMailOutline className="w-4 h-4 absolute right-4" />
+              </div>
             </div>
+            <button
+              type="submit"
+              className='px-2 py-1 text-sm font-semibold rounded text-white bg-primaryGreen hover:opacity-90 focus:outline-none'
+            >
+              Se connecter
+            </button>
+          </form>
           </div>
-          <button
-            type="submit"
-            className='px-2 py-1 text-sm font-semibold rounded text-white bg-primaryGreen hover:opacity-90 focus:outline-none'
-          >
-            Se connecter
-          </button>
-        </form>
-      </Modal.Body>
-    </Modal>
+        </Modal.Body>
+      </Modal>
+    </div>
   );
 };
 
