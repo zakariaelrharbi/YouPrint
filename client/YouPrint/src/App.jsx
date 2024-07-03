7a9d669c051af2585105dbeb63f3c7110f05a13e
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-07-03 12:47:13 +0100

    update

--- client/YouPrint/src/App.jsx
+++ client/YouPrint/src/App.jsx
@@ -3,7 +3,6 @@ import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Toaster } from 'sonner'; // Ensure you're using 'sonner' consistently
-import WatssapWidget from './components/WatssapWidget';
 
 const App = () => {
   return (
