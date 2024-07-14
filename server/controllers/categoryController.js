f207667dcf5ff39a1e7555c30916886d642b87ac
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-07-14 16:47:33 +0100

    creating create new category function

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -1,3 +1,25 @@
-const categorySchema = require('../models/categoryModal');
+const Category = require('../models/categoryModal');
 
 // Create a new category
+const createCategory = async (req, res) =>{
+    const {catergoryName, description, image} = req.body;
+    if(!catergoryName || !description){
+        return res.status(400).json({
+            message: 'All field are required',
+            error: true,
+            success: false,
+        })
+    }
+
+    try {
+        const newCategory = new Category ({catergoryName, description, image});
+        await newCategory.save();
+        return res.status(200).json({
+            message: 'category created successfully',
+            error: false,
+            success: true,
+        })
+    } catch (error) {
+        
+    }
+}
