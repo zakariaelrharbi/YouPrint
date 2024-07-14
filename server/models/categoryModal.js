87165c1c30b8b08f198b83337d4b6ac252f1cf77
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-07-14 16:17:30 +0100

    adding category schema

---
+++ server/models/categoryModal.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const categorySchema = new mongoose.Schema({
+    categoryName: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: true });
+
+const Category = mongoose.model('Category', categorySchema);
+module.exports = Category;
