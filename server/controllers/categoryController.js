7e81a1d89a43c0d8e0986087e6271a136d1a3afe
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-07-14 18:12:28 +0100

    update category function done

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -31,8 +31,29 @@ const createCategory = async (req, res) =>{
 const updateCategory = async (req, res) =>{
     const category_Id = req.params.id;
     const {categoryName, description}= req.body;
-    
+    try {
+        const category = await Category.findById(category_Id);
+        if (!category){
+            return res.status(404).json({
+                message: 'category not found',
+                error: true,
+                success: false,
+            })
+        }
+        await Category.findByIdAndUpdate(category_Id, {categoryName, description});
+        return res.status(200).json({
+            message: 'category updated successfully',
+            error: false,
+            success: true,
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message,
+            error: true,
+            success: false,
+        })
+    }
 }
 
-module.exports = {createCategory};
+module.exports = {createCategory, updateCategory};
 
--- server/routes/categoryRoutes.js
+++ server/routes/categoryRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const {createCategory} = require('../controllers/categoryController');
+const {createCategory, updateCategory} = require('../controllers/categoryController');
 
 const categoryRouter = express.Router();
 
 categoryRouter.post('/create-category', createCategory);
 // categoryRouter.get('/getAllCategory', getAllCategory);
 // categoryRouter.get('/getCategoryById/:id', getCategoryById);
-// categoryRouter.put('/update-category/:id', updateCategory);
+categoryRouter.put('/update-category/:id', updateCategory);
 // categoryRouter.delete('/delete-category/:id', deleteCategory);
 
 module.exports = categoryRouter;
