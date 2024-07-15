771641492488c5652b1e7508b65879d1aac943ae
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-07-15 17:39:15 +0100

    creating user routes

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const userRouter = express.Router();
+
+userRouter.post('/create-user', createUser);
+userRouter.get('/getAllUser', getAllUser);
+userRouter.get('/getUserById/:id', getUserById);
+userRouter.put('/update-user/:id', updateUser);
+userRouter.delete('/delete-user/:id', deleteUser);
+
+module.exports = userRouter;
