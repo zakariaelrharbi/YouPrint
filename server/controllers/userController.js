22119b3e33d6464e4bf4ffcf5c8ff61df8ca3fbc
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-07-15 18:32:25 +0100

    create new user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,48 @@
 const User = require('../models/userModel');
+const bcrypt = require('bcrypt');
 
+// create user
+ const createUser = async (req, res) => {
+   const { username, email, password } = req.body;
+
+    if (!username || !email || !password || username === '' || email === '' || password === '') {
+        return res.status(400).json({
+            message: 'All fields are required',
+            error: true,
+            success: false,
+        });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        return res.status(400).json({ 
+            message: 'User already exists',
+            error: true,
+            success: false,
+        });
+    }
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword,
+        });
+        await newUser.save();
+        return res.status(200).json({
+            message: 'User created successfully',
+            error: false,
+            success: true,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message,
+            error: true,
+            success: false,
+        });
+    }
+}
 
 // get all users
 const getAllUser = async (req, res) => {
@@ -14,6 +57,7 @@ const getAllUser = async (req, res) => {
             });
         }
         return res.status(200).json({
+            message: 'users found successfully', 
             users,
             error: false,
             success: true,
@@ -28,4 +72,4 @@ const getAllUser = async (req, res) => {
 };
 
 
-module.exports = {getAllUser};
+module.exports = {getAllUser, createUser};
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const {getAllUser } = require('../controllers/userController');
+const {createUser ,getAllUser } = require('../controllers/userController');
 
 const userRouter = express.Router();
 
-// userRouter.post('/create-user', createUser);
+userRouter.post('/create-user', createUser);
 userRouter.get('/getAllUser', getAllUser);
 // userRouter.get('/getUserById/:id', getUserById);
 // userRouter.put('/update-user/:id', updateUser);
